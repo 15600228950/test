2cbb7174245ebc48d242448a484b8580f12fbbea
MrLi <1356322517@qq.com>
2019-06-23 14:13:05 +0800

    第一次提交

---
+++ test/src/com/gittest.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * @author ：XinLi
+ * @date ：Created in 2019/6/23 14:10
+ * @description ：
+ * @version: 1.0
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+        System.out.println("大家好");
+    }
+}
